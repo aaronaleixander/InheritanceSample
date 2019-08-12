5e15fb14749c5851bfb9e0ca79e2732e92e32e34
Aaron Utterback <aar0nu@outlook.com>
2019-08-12 14:45:37 -0700

    Made the Player, Enemy, and Creature Class in the creatures file.

--- InheritanceSample/Creatures.cs
+++ InheritanceSample/Creatures.cs
@@ -8,19 +8,28 @@ namespace InheritanceSample
 {
     // Use a colon : for inheritance
 
-
+    /// <summary>
+    /// A base class to represent human and AI controlled characters.
+    /// </summary>
     class Creature
     {
-
+        public string Name { get; set; }
+        public double Health { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Speed { get; set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     class Player : Creature
     {
-
+        public double Stamina { get; set; }
     }
 
     class Enemy : Creature
     {
-
+        public int EnemyId { get; set; }
     }
 }
--- InheritanceSample/Program.cs
+++ InheritanceSample/Program.cs
@@ -10,6 +10,25 @@ namespace InheritanceSample
     {
         static void Main(string[] args)
         {
+            Player p1 = new Player();
+            p1.Attack = 100;
+            p1.Name = "Link";
+            p1.Health = 20.75;
+            p1.Stamina = 9.99;
+
+            Enemy e = new Enemy();
+            e.EnemyId = 1;
+            e.Defense = 100;
+
+            Creature c = new Creature();
+
+            Creature c2 = new Player();
+
+            // How can we set stamina when we know the creature is a player
+            
+
+            
+
         }
     }
 }
