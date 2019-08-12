f7bb04a73b8148c4c11d2b0db44722c3a746bacb
Aaron Utterback <aar0nu@outlook.com>
2019-08-12 14:28:30 -0700

    Add project files.

---
+++ InheritanceSample/Creatures.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSample
+{
+    // Use a colon : for inheritance
+
+
+    class Creature
+    {
+
+    }
+
+    class Player : Creature
+    {
+
+    }
+
+    class Enemy : Creature
+    {
+
+    }
+}
